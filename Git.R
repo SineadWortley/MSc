c04b4d8d06c80a56fab41e05555514736274b6b2
SineadWortley <u18122672@tuks.co.za>
2025-06-24 13:40:30 +0200

    My first commit

---
+++ Git.R
@@ -0,0 +1,13 @@
+install.packages("usethis")
+library(usethis)
+install.packages("gitcreds")
+library(gitcreds)
+usethis::use_git_config(
+  user.name = "SineadWortley", 
+  user.email = "u18122672@tuks.co.za"
+)
+usethis::create_github_token()
+gitcreds::gitcreds_set()
+
+usethis::use_git()
+1
